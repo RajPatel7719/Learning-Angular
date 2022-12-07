5631dec6876836daff166a8beb9c4b0726ada016
Raj <tbs.rajg@gmail.com>
2022-12-07 15:43:46 +0530

    Project Initialization

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  //  TODO  :   To use the TAG selector
+  selector: 'app-server',
+
+  //  TODO  :   To use the ATTRIBUTE selector
+  // selector: '[app-server]',
+
+  //  TODO  :   To use the CLASS selector
+  // selector: '.app-server',
+
+  //  TODO  :   To use EXTERNAL HTML file
+  templateUrl: './server.component.html',
+
+  //  TODO  :   To use INLINE HTML file
+  // template: `
+  // <h3> I'm from inline template...!</h3>
+  // `,
+
+  //  TODO  :   To use external CSS file
+  // styleUrls: ['./server.component.css'],
+
+  //  TODO  :   To use INLINE CSS file
+  styles: [`
+    h1{
+      color: dodgerblue;
+    }
+  `]
+})
+
+export class ServerComponent {
+
+}
