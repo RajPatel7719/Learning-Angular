8cf8bb97aa3b3c1d20e136fd48fff93675699731
Raj <tbs.rajg@gmail.com>
2022-12-12 09:41:55 +0530

    implement Routing Concepts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Server1Component } from './servers/server/server.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,6 +13,27 @@ import { ServerElementComponent } from './server-element/server-element.componen
 import { CustomDirectiveDirective } from './directives/custom-directive.directive';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { UserComponent } from './users/user/user.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent },
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: Server1Component },
+      { path: ':id/edit', component: EditServerComponent },
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
@@ -23,12 +45,19 @@ import { NewAccountComponent } from './new-account/new-account.component';
     ServerElementComponent,
     CustomDirectiveDirective,
     AccountComponent,
-    NewAccountComponent
+    NewAccountComponent,
+    HomeComponent,
+    UsersComponent,
+    UserComponent,
+    ServersComponent,
+    EditServerComponent,
+    Server1Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, ActivatedRoute, Params, Router } from '@angular/router';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-server1',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class Server1Component implements OnInit {
+  server: { id: number, name: string, status: string };
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    })
+  }
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route })
+  }
+}
